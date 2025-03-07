ca777d9edd81b240351e369bb9649368514403b6
nirajnirsingh <csaiml22131@glbitm.ac.in>
2025-03-07 20:23:47 +0530

    Time: 309 ms (32.96%) | Memory: 23.5 MB (30.18%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,33 +1,77 @@
 class Solution {
 public:
+/////////////////////////////////////////////////////////////////// 
+    //BRUTE FORCE APPROACH 
 
-    bool isprime(int num){
 
-        int i=2;
-        while(i <= sqrt(num)){
-            if(num%i == 0){
-                return false;
+    // bool isprime(int num){
+
+    //     int i=2;
+    //     while(i <= sqrt(num)){
+    //         if(num%i == 0){
+    //             return false;
+    //         }
+    //         i++;
+    //     }
+    //     return true;
+    // }
+
+    // vector<int> closestPrimes(int left, int right) {
+        
+    //     int i = left;
+    //     vector<int>primes;
+        
+    //     if(left == 1) left++;
+    //     for(int i=left; i<=right; i++){
+    //         if(isprime(i)){
+    //             primes.push_back(i);
+    //         }
+    //     }
+    //     vector<int>pair(2);
+    //     int mindiff = INT_MAX;
+    //     if(primes.size()<2) return {-1,-1};
+        
+    //     for(int i=1; i<primes.size(); i++){
+
+    //         if(mindiff > primes[i]-primes[i-1]){
+    //             mindiff = primes[i]-primes[i-1];
+    //             pair[0] = primes[i-1];
+    //             pair[1] = primes[i];
+    //         }
+    //     }
+
+    //     if(pair[0] != -1 && pair[1] != -1)
+    //         return {pair[0],pair[1]};
+
+    //         return {-1,-1};
+    // }
+
+    vector<int> closestPrimes(int left, int right){
+
+        vector<bool>isPrime(right+1,true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i=2; i<right; i++){
+           
+           if( isPrime[i]){
+
+            for(int j=2; i*j<= right; j++){
+
+                isPrime[i*j] = false;
             }
-            i++;
+           }
         }
-        return true;
-    }
-
-    vector<int> closestPrimes(int left, int right) {
-        
-        int i = left;
+        int mindiff = INT_MAX;
+        vector<int>pair(2);
         vector<int>primes;
-        
-        if(left == 1) left++;
-        for(int i=left; i<=right; i++){
-            if(isprime(i)){
+       for(int i=left; i <= right; i++){
+            if(isPrime[i]){
                 primes.push_back(i);
             }
         }
-        vector<int>pair(2);
-        int mindiff = INT_MAX;
-        if(primes.size()<2) return {-1,-1};
-        
+         if(primes.size()<2) return {-1,-1};
         for(int i=1; i<primes.size(); i++){
 
             if(mindiff > primes[i]-primes[i-1]){
@@ -38,7 +82,7 @@ public:
         }
         if(pair[0] != -1 && pair[1] != -1)
             return {pair[0],pair[1]};
-
-            return {-1,-1};
+        return {-1,-1};
     }
+
 };

e1b67a0e8cf5f238913c06a951d065f0d05f0127
nirajnirsingh <csaiml22131@glbitm.ac.in>
2025-03-19 15:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.62%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
-    int maxrob(vector<int>& nums, int index, int end,vector<int>& dp) {
 
-        if (index > end) {
-            // ans.push_back(sum);
-            return 0;
-        }
-        if(index == end) return nums[index];
+    // --------memoisation-----------
 
-        if (dp[index] != -1)
-            return dp[index];
+    // int maxrob(vector<int>& nums, int index, int end,vector<int>& dp)    {
 
-        return dp[index] = max(maxrob(nums, index + 1, end, dp),
-                     (nums[index] + maxrob(nums, index + 2, end,dp)));
-    }
+    //     if (index > end) {
+    //       
+    //         return 0;
+    //     }
+    //     if(index == end) return nums[index];
+
+    //     if (dp[index] != -1)
+    //         return dp[index];
+
+    //     return dp[index] = max(maxrob(nums, index + 1, end, dp),
+    //                  (nums[index] + maxrob(nums, index + 2, end,dp)));
+    // }
+
+    //-------tabulation------
+
     int rob(vector<int>& nums) {
 
         int n = nums.size();
-        
-        vector<int> ans;
+
         vector<int>dp(n,-1);
+        if(n==1) return nums[0];
 
-      return   maxrob(nums, 0, n-1, dp);
+        dp[n-1] = nums[n-1];
+        dp[n-2] = max(nums[n-1],nums[n-2]);
 
+        for(int i = n-3; i>=0; i--){
+            dp[i] = max(dp[i+1] , nums[i]+dp[i+2]);
+        }
+      return   max(dp[0] , dp[1]);
     }
 };
